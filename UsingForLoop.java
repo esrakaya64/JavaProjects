509628329b0410d18281b94aa141f4a090ac3123
Esra Kaya <112573124+esrakaya64@users.noreply.github.com>
2022-10-01 11:01:55 +0300

    Add files via upload
    Basic form of using for loops.

---
+++ UsingForLoop.java
@@ -0,0 +1,17 @@
+
+package usingforloop;
+
+
+public class UsingForLoop {
+
+    
+    public static void main(String[] args) {
+        /*Basit bir for döngüsü kullanımı
+        Burada 1'den 15'e kadar olan sayıları yazdırmak istiyoruz.
+        Öncelikle değişkenimize 1 Değerini veriyoruz.
+        Değişkenimiz 16'dan küçük olduğu sürece devamlı artsın istiyoruz ve ekrana yazdırıyoruz.*/
+        for(int i = 1; i<16; i++)
+            System.out.println(i);
+    }
+    
+}
