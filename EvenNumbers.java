81501d9f8d4a29a936697d5fd7afbd98ae839e9e
Esra Kaya <112573124+esrakaya64@users.noreply.github.com>
2022-10-04 14:04:25 +0300

    Add files via upload
    Even numbers.

---
+++ EvenNumbers.java
@@ -0,0 +1,19 @@
+
+package evennumbers;
+
+
+public class EvenNumbers {
+
+   
+    public static void main(String[] args) {
+      //We want to numbers can be divided by 2 without remainder
+      //We should a stop number for stop loop
+      for(int i =0; i<89; i++){
+          if(i%2 == 00){
+              System.out.println(i);
+          }
+      }
+        
+    }
+    
+}
